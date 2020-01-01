ead0acdeac348db118a8995a34b1710d068da3b2
michael eze <michael.eze@diconium.com>
2020-01-01 22:53:13 +0100

    clean code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../../components/header';
-import logo from '../../assets/images/logo.svg';
 import './index.css';
 
 const Home: React.FC = () => <Header />
