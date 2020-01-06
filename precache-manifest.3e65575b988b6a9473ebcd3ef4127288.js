7ced8f4c0c01fb70a305c5f9a48d540db9488c39
michaeleze <michaeleze3@gmail.com>
2020-01-06 21:33:33 +0100

    Updates

---
+++ precache-manifest.3e65575b988b6a9473ebcd3ef4127288.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d660f78ec951e922d34ef87f0b8ffe06",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7a0da34a08272b986b37",
+    "url": "/portfolio/static/css/main.4116f4e6.chunk.css"
+  },
+  {
+    "revision": "f3a7a95c3afe9ef0c62e",
+    "url": "/portfolio/static/js/2.d8d5f5d0.chunk.js"
+  },
+  {
+    "revision": "7a0da34a08272b986b37",
+    "url": "/portfolio/static/js/main.1416cccc.chunk.js"
+  },
+  {
+    "revision": "c48c336fb9566862a932",
+    "url": "/portfolio/static/js/runtime-main.63fad287.js"
+  }
+]);
